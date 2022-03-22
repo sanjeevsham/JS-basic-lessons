882c6d1083d86366b332b01c97ef724092ceeb74
sanjeevsham <sanjeev.neelakantan@chainsys.com>
2022-03-22 13:39:15 +0530

    commit

--- challenges/time-tracking-dashboard-main/time traking.js
+++ challenges/time-tracking-dashboard-main/time traking.js
@@ -1,170 +1,71 @@
-/* eslint-disable no-unused-expressions */
-const data = [
-  {
-    title: 'Work',
-    link: '',
-    timeframes: {
-      daily: {
-        current: 5,
-        previous: 7
-      },
-      weekly: {
-        current: 32,
-        previous: 36
-      },
-      monthly: {
-        current: 103,
-        previous: 128
-      }
-    }
-  },
-  {
-    title: 'Play',
-    link: './icon-play.svg',
-    timeframes: {
-      daily: {
-        current: 1,
-        previous: 2
-      },
-      weekly: {
-        current: 10,
-        previous: 8
-      },
-      monthly: {
-        current: 23,
-        previous: 29
-      }
-    }
-  },
-  {
-    title: 'Study',
-    link: './icon-study.svg',
-    timeframes: {
-      daily: {
-        current: 0,
-        previous: 1
-      },
-      weekly: {
-        current: 4,
-        previous: 7
-      },
-      monthly: {
-        current: 13,
-        previous: 19
-      }
-    }
-  },
-  {
-    title: 'Exercise',
-    link: './icon-exercise.svg',
-    timeframes: {
-      daily: {
-        current: 1,
-        previous: 1
-      },
-      weekly: {
-        current: 4,
-        previous: 5
-      },
-      monthly: {
-        current: 11,
-        previous: 18
-      }
-    }
-  },
-  {
-    title: 'Social',
-    link: './icon-social.svg',
-    timeframes: {
-      daily: {
-        current: 1,
-        previous: 3
-      },
-      weekly: {
-        current: 5,
-        previous: 10
-      },
-      monthly: {
-        current: 21,
-        previous: 23
-      }
-    }
-  },
-  {
-    title: 'Self Care',
-    link: './icon-self-care.svg',
-    timeframes: {
-      daily: {
-        current: 0,
-        previous: 1
-      },
-      weekly: {
-        current: 2,
-        previous: 2
-      },
-      monthly: {
-        current: 7,
-        previous: 11
-      }
-    }
-  }
-]
-const tasksHtml = document.querySelector('.menu-tasks')
-const periodHtml = document.querySelectorAll('.period nav ul li')
-function updateTBoxes () {
-  tasksHtml.innerHTML = ''
-  data.forEach((obj) => {
-    tasksHtml.innerHTML += `
-      <div class="task-box">
-         <div class="task-box-hat"><img src="${obj.link}" alt="${obj.title}"></div>
-         <div class="task-box-content">
-              <div class="task-box-header">
-              <h2>${obj.title}</h2>
-              <button class="more-options" aria-label="options"><img src="./icon-ellipsis.svg" alt="ellipsis"></button>
-         </div>
-              <div class="task-box-info">
-                  <h3 class="total-hours">${obj.timeframes.daily.current}hrs</h3>
-                  <p> Yesterday - ${obj.timeframes.daily.previous}hrs</p>
-              </div>
-          </div>
-       </div>`
-  })
-}
-updateTBoxes()
-const infoHtml = document.querySelectorAll('.task-box-info')
+let dailybtn = document.getElementById("daily")
+let weeklybtn = document.getElementById("weekly")
+let monthlybtn = document.getElementById("monthly")
 
-function updatePeriod (period) {
-  period = period.toLowerCase()
-  let pMsg
-  switch (period) {
-    case 'daily':
-      pMsg = 'Yesterday'
-      break
-    case 'weekly':
-      pMsg = 'Last Week'
-      break
-    case 'monthly':
-      pMsg = 'Last Month'
-      break
-  }
-  infoHtml.forEach((item, i) => {
-    item.style.animation = ''
-    setTimeout(() => {
-      item.style.animation = 'changePeriod 0.3s linear'
-      item.querySelector(
-        'h3'
-      ).innerText = `${data[i].timeframes[period].current}hrs`
-      item.querySelector(
-        'p'
-      ).innerText = `${pMsg} - ${data[i].timeframes[period].previous}hrs`
-    }, 40)
-  })
-}
+let daily_Data = document.getElementsByClassName("daily-data")
+let daily_Data_Previous = document.getElementsByClassName("daily-data-previous")
 
-periodHtml.forEach((html) => {
-  html.addEventListener('click', () => {
-    html.classList === '' ? updatePeriod(html.innerText) : 0
-    periodHtml.forEach((item) => (item.classList = ''))
-    html.classList += 'active'
-  })
-})
+
+let weekly_Data = document.getElementsByClassName("weekly-data")
+let weekly_Data_Previous = document.getElementsByClassName("weekly-data-previous")
+
+
+let monthly_Data = document.getElementsByClassName("monthly-data")
+let monthly_Data_Previous = document.getElementsByClassName("monthly-data-previous")
+
+
+
+function show_based_time_data() {
+    dailybtn.addEventListener("click", function() {
+        dailybtn.style.cssText = "color:white"
+        weeklybtn.style.cssText = "color: hsl(235, 45%, 61%)"
+        monthlybtn.style.cssText = "color: hsl(235, 45%, 61%)"
+
+        for (let i = 0; i < 6; i++) {
+            daily_Data[i].style.cssText = "display:block"
+            daily_Data_Previous[i].style.cssText = "display:block"
+
+            weekly_Data[i].style.cssText = "display:none"
+            weekly_Data_Previous[i].style.cssText = "display:none"
+
+            monthly_Data[i].style.cssText = "display:none"
+            monthly_Data_Previous[i].style.cssText = "display:none"
+        }
+
+    })
+    weeklybtn.addEventListener("click", function() {
+        dailybtn.style.cssText = "color: hsl(235, 45%, 61%)"
+        weeklybtn.style.cssText = "color:white"
+        monthlybtn.style.cssText = "color: hsl(235, 45%, 61%)"
+
+        for (let i = 0; i < 6; i++) {
+            daily_Data[i].style.cssText = "display:none"
+            daily_Data_Previous[i].style.cssText = "display:none"
+
+            weekly_Data[i].style.cssText = "display:block"
+            weekly_Data_Previous[i].style.cssText = "display:block"
+
+            monthly_Data[i].style.cssText = "display:none"
+            monthly_Data_Previous[i].style.cssText = "display:none"
+        }
+
+
+    })
+    monthlybtn.addEventListener("click", function() {
+        dailybtn.style.cssText = "color: hsl(235, 45%, 61%)"
+        weeklybtn.style.cssText = "color: hsl(235, 45%, 61%)"
+        monthlybtn.style.cssText = "color:white"
+
+        for (let i = 0; i < 6; i++) {
+            daily_Data[i].style.cssText = "display:none"
+            daily_Data_Previous[i].style.cssText = "display:none"
+
+            weekly_Data[i].style.cssText = "display:none"
+            weekly_Data_Previous[i].style.cssText = "display:none"
+
+            monthly_Data[i].style.cssText = "display:block"
+            monthly_Data_Previous[i].style.cssText = "display:block"
+        }
+    })
+}
+show_based_time_data()
