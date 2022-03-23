42a935ece2ddf72b381fb0000c09011e8f3e203e
sanjeevsham <sanjeev.neelakantan@chainsys.com>
2022-03-23 11:33:11 +0530

    commit

--- challenges/article-preview-component-master/artical.js
+++ challenges/article-preview-component-master/artical.js
@@ -6,10 +6,11 @@ let open = false
 
 function share () {
   open = !open
-  if (open) {
+   if (open) {
     share_badge.classList.remove('off')
     share_badge.classList.add('on')
-  } else {
+  } else 
+  {
     share_badge.classList.add('off')
     share_badge.classList.remove('on')
   }
